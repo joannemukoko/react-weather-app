b2bc25ccd668b0e7a78e0eafd42c2c3291bcd16c
joannemukoko <97846226+joannemukoko@users.noreply.github.com>
2022-05-10 17:20:37 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,5 +3,24 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by {""}{" "}
+          <a href="https://www.linkedin.com/in/joannemukoko/" target="_blank">
+            Joanne Mukoko
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/joannemukoko/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
