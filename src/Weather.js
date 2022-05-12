762205449af62910b62abe079876c06d78f19a9f
joannemukoko <97846226+joannemukoko@users.noreply.github.com>
2022-05-12 16:32:44 +0100

    Removed API integration as it was not working

--- src/Weather.js
+++ src/Weather.js
@@ -1,76 +1,52 @@
-import React, { useState } from "react";
-import axios from "axios";
 import "./Weather.css";
 import ReactAnimatedWeather from "react-animated-weather";
 
-export default function Weather(props) {
-  const [weatherData, setWeatherData] = useState({ ready: false });
-  function handleResponse(response) {
-    setWeatherData({
-      ready: true,
-      temperature: response.data.main.temp,
-      wind: response.data.wind.speed,
-      city: response.data.name,
-      humidity: response.data.main.humidity,
-      description: response.data.weather[0].description,
-    });
-  }
-
-  if (weatherData.ready) {
-    return (
-      <div className="Weather">
-        <form>
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Enter a city"
-                className="form-control"
-                autoFocus="on"
-              />
-            </div>
-            <div className="col-3">
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-primary w-100"
-              />
-            </div>
-          </div>
-        </form>
-        <h1>{weatherData.city}</h1>
-        <ul className="description">
-          <li>Tuesday 10th May </li>
-          <li>17:27 </li>
-          <li className="text-capitalize">{weatherData.description} </li>
-        </ul>
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
         <div className="row">
-          <div className="col-6">
-            <ReactAnimatedWeather
-              icon="PARTLY_CLOUDY_DAY"
-              color="#0969da"
-              size={50}
-              animate={true}
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city"
+              className="form-control"
+              autoFocus="on"
             />
-            <span className="temperature">
-              {Math.round(weatherData.temperature)}
-            </span>{" "}
-            <span className="units">°C</span>
           </div>
-          <div className="col-6">
-            <ul className="wind">
-              <li> Humidity: {weatherData.humidity}%</li>
-              <li> Wind: {Math.round(weatherData.wind)} km/h</li>
-            </ul>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
+      </form>
+      <h1>London</h1>
+      <ul className="description">
+        <li>Tuesday 10th May </li>
+        <li>17:27 </li>
+        <li className="text-capitalize">cloudy </li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <ReactAnimatedWeather
+            icon="PARTLY_CLOUDY_DAY"
+            color="#0969da"
+            size={50}
+            animate={true}
+          />
+          <span className="temperature">6</span>{" "}
+          <span className="units">°C</span>
+        </div>
+        <div className="col-6">
+          <ul className="wind">
+            <li> Humidity: 40%</li>
+            <li> Wind: 2 km/h</li>
+          </ul>
+        </div>
       </div>
-    );
-  } else {
-    const apiKey = "78f7c5478076af4eadd670db8e28a4b6";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&units=metric&appid=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
-
-    return "Loading..";
-  }
+    </div>
+  );
 }
