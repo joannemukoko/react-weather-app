32ebe849f2a32e1e52bfa2890065c13c7c379c57
joannemukoko <97846226+joannemukoko@users.noreply.github.com>
2022-05-12 15:05:04 +0100

    Added API integration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defualtCity="London" />
         <footer>
           This project was coded by {""}{" "}
           <a href="https://www.linkedin.com/in/joannemukoko/" target="_blank">
