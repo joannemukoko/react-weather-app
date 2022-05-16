a6dcf09337881e567ddb107133dd68ba389af172
joannemukoko <97846226+joannemukoko@users.noreply.github.com>
2022-05-16 15:53:34 +0100

    Added unit conversation

---
+++ src/WeatherTemperature.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+export default function WeatherTemperature(props) {
+  const [unit, setUnit] = useState("celsuis");
+  function showFarenheit(event) {
+    event.preventDefault();
+    setUnit("farenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  function farenheit() {
+    return (props.celsius * 9) / 5 + 32;
+  }
+
+  if (unit === "celsuis") {
+    return (
+      <div className="WeatherTemperature">
+        <span className="temperature">{Math.round(props.celsius)}</span>
+        <span className="units">
+          °C |{" "}
+          <a href="/" onClick={showFarenheit}>
+            °F
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+    return (
+      <div className="WeatherTemperature">
+        <span className="temperature">{Math.round(farenheit())}</span>
+        <span className="units">
+          <a href="/" onClick={showCelsius}>
+            °C{" "}
+          </a>
+          | °F
+        </span>
+      </div>
+    );
+  }
+}
