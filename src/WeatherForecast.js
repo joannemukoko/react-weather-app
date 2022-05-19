8f1c286320e88b183eb6ede6a5fdf2d19fbf2e81
joannemukoko <97846226+joannemukoko@users.noreply.github.com>
2022-05-19 16:02:39 +0100

    Changed the technique of displaying 5 day forecast because using the loop way would not deploy to netlify

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,15 +16,21 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          {forecast.map(function (dailyForecast, index) {
-            if (index < 5) {
-              return (
-                <div className="col" key={index}>
-                  <WeatherForecastDay data={dailyForecast} />
-                </div>
-              );
-            }
-          })}
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
+          </div>
+          <div className="col">
+            <WeatherForecastDay data={forecast[1]} />
+          </div>
+          <div className="col">
+            <WeatherForecastDay data={forecast[2]} />
+          </div>
+          <div className="col">
+            <WeatherForecastDay data={forecast[3]} />
+          </div>
+          <div className="col">
+            <WeatherForecastDay data={forecast[4]} />
+          </div>
         </div>
       </div>
     );
