87fea9c01b6e5180afe890005f5b0f74913136e4
joannemukoko <97846226+joannemukoko@users.noreply.github.com>
2022-05-20 15:04:29 +0100

    Removed fahrenheit feature

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,44 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celsius");
-  function showFarenheit(event) {
-    event.preventDefault();
-    setUnit("farenheit");
-  }
-
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  function farenheit() {
-    return (props.celsius * 9) / 5 + 32;
-  }
-
-  if (unit === "celsius") {
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(props.celsius)}</span>
-        <span className="units">
-          °C |{" "}
-          <a href="/" onClick={showFarenheit}>
-            °F
-          </a>
-        </span>
-      </div>
-    );
-  } else {
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(farenheit())}</span>
-        <span className="units">
-          <a href="/" onClick={showCelsius}>
-            °C{" "}
-          </a>
-          | °F
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="WeatherTemperature">
+      <span className="temperature">{Math.round(props.celsius)}</span>
+      <span className="units">°C</span>
+    </div>
+  );
 }
